3290989443abc81f6d9f017227ebca22bf22ce36
Diana Nazarova <alllleksandrova@mail.ru>
2023-05-30 20:49:45 +0400

    added cors packet

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const router = require('./routes/index');
 const { createUser, login } = require('./controllers/auth');
 const auth = require('./middlewares/auth');
@@ -12,6 +13,7 @@ const {
 } = process.env;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(
   express.urlencoded({ extended: true }),
